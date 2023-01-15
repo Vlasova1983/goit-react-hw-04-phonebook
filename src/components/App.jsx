af4e2a95774233820eba63533c0c9eb6803a2a66
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-15 09:17:34 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import {Component} from "react";
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+import styles  from './App.module.css';
+
+
+export class App  extends Component {
+  state = {
+    contacts: [],
+    filter: '',        
+  };
+
+  componentDidMount() { 
+    if (this.state.contacts.length!==0) {
+      const contacts = JSON.parse(localStorage.getItem('contacts'))
+      this.setState({ contacts });
+    }       
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contact !==  this.state.contacts){    
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }    
+  }
+
+  isContactInState = ({ name }) =>
+    !!this.state.contacts.filter(({name: prevName}) => {
+      return prevName === name 
+  }).length;
+
+  handleSubmit = ({ name, number }) => { 
+    if (this.isContactInState({ name })) {
+      alert('Contact is in phonebook');
+      return;    
+    }
+
+    this.setState(({ contacts: prevState }) => ({
+      contacts: [...prevState, { id:this.getRandomID(), name, number }],
+    }));   
+  };
+
+  handleFilterContacts =(value)=>{ 
+    this.setState({filter:value});       
+  };
+
+  getFilterContact() {
+    return this.state.contacts.filter((contact)=>contact.name.toLowerCase().includes(this.state.filter));
+  }
+
+
+  handleDeleteContact =(id)=>{   
+   this.setState(prevState=>{   
+    return{contacts:prevState.contacts.filter((contact)=>contact.id !== id)};
+    });   
+  };
+
+  getRandomID() {
+    return `${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+  render(){ 
+    const   {filter} = this.state;
+      
+    return (
+      <div className={styles.conteiner}>
+        <h1>Phonebook</h1>
+        <ContactForm   onSubmit={this.handleSubmit}/>
+
+        <h2>Contacts</h2>
+        <Filter filter={filter} onFilter={this.handleFilterContacts}/>
+        <ContactList contacts={this.getFilterContact()} onDelete={this.handleDeleteContact}/>               
+      </div>
+    );
+  }   
 };
