2d02eaa96ca5835de6dfcf94257d48adbd28daec
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-15 17:23:58 +0200

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,20 +5,19 @@ import styles  from '../Contact/Contact.module.css';
 export const Contact = ({contact,onDelete}) => {
   const {id,name,number} = contact;
 
-    const handleDelete =() =>{
-        onDelete(id);
-    };
-
-    return (        
-        <div className={styles.link}>
-            <li> {name} : {number}</li> 
-            <button className={styles.button} onClick={handleDelete}>Delete</button>
-        </div>                    
-                   
-    );
+  const handleDelete =() =>{
+    onDelete(id);
   };
 
-  Contact.propTypes = {         
-    contact:  PropTypes.shape(),
-    onDelete:PropTypes.func
+  return (        
+    <div className={styles.link}>
+      <li> {name} : {number}</li> 
+      <button className={styles.button} onClick={handleDelete}>Delete</button>
+    </div>                  
+  );
+};
+
+Contact.propTypes = {         
+  contact:  PropTypes.shape(),
+  onDelete:PropTypes.func
 }
