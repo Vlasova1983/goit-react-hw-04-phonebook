5b47100df536c1847cacc6ec1d9f25303bea90cc
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-15 17:17:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import {Component} from "react";
+import {useState,useEffect} from "react";
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
@@ -6,72 +6,56 @@ import { ContactList } from './ContactList/ContactList';
 import styles  from './App.module.css';
 
 
-export class App  extends Component {
-  state = {
-    contacts: [],
-    filter: '',        
-  };
+export const App  =()=> {
+  
+  const [contacts,setInContacts] = useState([]);
+  const [filter,setInFilter] = useState('');
 
-  componentDidMount() { 
-    if (this.state.contacts.length!==0) {
-      const contacts = JSON.parse(localStorage.getItem('contacts'))
-      this.setState({ contacts });
-    }       
-  }
+  useEffect(()=>{     
+    const contacts = JSON.parse(localStorage.getItem('contacts'));      
+    setInContacts(contacts);       
+  },[]);
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contact !==  this.state.contacts){    
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }    
-  }
+  useEffect(()=>{      
+    localStorage.setItem('contacts', JSON.stringify(contacts));          
+  },[contacts]); 
 
-  isContactInState = ({ name }) =>
-    !!this.state.contacts.filter(({name: prevName}) => {
-      return prevName === name 
-  }).length;
+  const isContactInState = ({ name }) =>
+  !!contacts.filter(({name: prevName}) => {return prevName === name}).length;
 
-  handleSubmit = ({ name, number }) => { 
-    if (this.isContactInState({ name })) {
+  const handleSubmit = ({ name, number }) => { 
+    if (isContactInState({ name })) {
       alert('Contact is in phonebook');
       return;    
-    }
-
-    this.setState(({ contacts: prevState }) => ({
-      contacts: [...prevState, { id:this.getRandomID(), name, number }],
-    }));   
+    }   
+    setInContacts([...contacts, { id:getRandomID(), name, number}]);      
   };
 
-  handleFilterContacts =(value)=>{ 
-    this.setState({filter:value});       
+  const handleFilterContacts =(value)=>{ 
+    setInFilter(value);       
   };
 
-  getFilterContact() {
-    return this.state.contacts.filter((contact)=>contact.name.toLowerCase().includes(this.state.filter));
-  }
-
-
-  handleDeleteContact =(id)=>{   
-   this.setState(prevState=>{   
-    return{contacts:prevState.contacts.filter((contact)=>contact.id !== id)};
-    });   
+  const getFilterContact=()=> {    
+    return contacts.filter((contact)=>contact.name.toLowerCase().includes(filter));
   };
 
-  getRandomID() {
+
+  const handleDeleteContact =(id)=>{   
+    setInContacts(contacts.filter((contact)=>contact.id !== id));  
+    return contacts;    
+  };
+
+  const getRandomID=()=> {
     return `${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
-  
-  render(){ 
-    const   {filter} = this.state;
-      
-    return (
-      <div className={styles.conteiner}>
-        <h1>Phonebook</h1>
-        <ContactForm   onSubmit={this.handleSubmit}/>
-
-        <h2>Contacts</h2>
-        <Filter filter={filter} onFilter={this.handleFilterContacts}/>
-        <ContactList contacts={this.getFilterContact()} onDelete={this.handleDeleteContact}/>               
-      </div>
-    );
-  }   
+  };
+    
+  return (
+    <div className={styles.conteiner}>
+      <h1>Phonebook</h1>
+      <ContactForm   onSubmit={handleSubmit}/>
+      <h2>Contacts</h2>
+      <Filter filter={filter} onFilter={handleFilterContacts}/>
+      <ContactList contacts={getFilterContact()} onDelete={handleDeleteContact}/>               
+    </div>
+  );     
 };
